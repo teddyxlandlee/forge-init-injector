125317bcf3a84f1c466e31e044db9a450122b64b
Teddy Li <teddyxlandlee@hotmail.com>
2026-03-16 13:20:21 +0800

    Update Gradle wrapper

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,5 +4,10 @@ pluginManagement {
         maven("https://maven.aliyun.com/repository/gradle-plugin")
     }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "forge-init-injector"
 
